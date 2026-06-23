4c11c1dbfa5909b9049411d3c5a594bb76ee4a4d
Jasper van den Heuvel <jasperheuvel13@gmail.com>
2026-06-23 09:02:01 +0200

    React/Next front-end

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export', // Verplicht voor GitHub Pages: dwingt een statische HTML-export af
+  images: {
+    unoptimized: true, // Verplicht: GitHub Pages heeft geen server-side image optimization
+  },
 };
 
 export default nextConfig;
